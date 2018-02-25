620543c0af05b361f44a5c8d9021d514b66cabed
tdavidsonas88 <tdavidsonas88@gmail.com>
2018-02-25 18:43:12 +0200

    Create post functionality implemented

--- src/AppBundle/Controller/BlogPostsController.php
+++ src/AppBundle/Controller/BlogPostsController.php
@@ -4,6 +4,8 @@ namespace AppBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Symfony\Component\HttpFoundation\Request;
+use AppBundle\Form\BlogPostType;
 
 class BlogPostsController extends Controller
 {
@@ -18,6 +20,29 @@ class BlogPostsController extends Controller
         return $this->render('BlogPosts/list.html.twig', array(
             'blog_posts' => $blogPosts,
         ));
+    }
+
+    /**
+     * @param Request $request
+     * @Route("/create", name="create")
+     */
+    public function createAction(Request $request) {
+        // $blogPost = new BlogPost();
+        $form = $this->createForm(BlogPostType::class);
+        $form->handleRequest($request);
+
+        if($form->isSubmitted() && $form->isValid()) {
+            $blogPost = $form->getData();
+            $em = $this->getDoctrine()->getManager();
+            $em->persist($blogPost);
+            $em->flush();
+
+            return $this->redirectToRoute('list');
+        }
+
+        return $this->render('BlogPosts/create.html.twig', [
+            'form' => $form->createView()
+        ]);
     }
 
 }
